11c75631301a94c6f7a2a45f5ad305210b7929da
kgh <rjsgml302@naver.com>
2023-02-20 01:41:51 +0900

    1

--- src/components/main/shop_component/Shop.tsx
+++ src/components/main/shop_component/Shop.tsx
@@ -8,16 +8,16 @@ export const Shop = () => {
       <div className={Styled.shop_contents}>
 
         <div className={Styled.shop_items_list}>
-          <div className={Styled.shop_item}></div>
-          <div className={Styled.shop_item}></div>
-          <div className={Styled.shop_item}></div>
-          <div className={Styled.shop_item}></div>
+          <div className={Styled.shop_item}> 상품 1 </div>
+          <div className={Styled.shop_item}> 상품 2 </div>
+          <div className={Styled.shop_item}> 상품 3 </div>
+          <div className={Styled.shop_item}> 상품 4 </div>
         </div>
 
         <div className={Styled.shop_pagenation}>
-          <div className={`${Styled.carousel} ${Styled.carousel_left}`}></div>
-          <div className={`${Styled.carousel} ${Styled.carousel_no}`}></div>
-          <div className={`${Styled.carousel} ${Styled.carousel_right}`}></div>
+          <div className={`${Styled.carousel} ${Styled.carousel_left}`}> {"<"} </div>
+          <div className={`${Styled.carousel} ${Styled.carousel_no}`}> 1,2,3,4,5,6 </div>
+          <div className={`${Styled.carousel} ${Styled.carousel_right}`}> {">"} </div>
         </div>
       </div>
     </React.Fragment>
