fad7335b5b4917075113f2a5f0a61788a6e3ab46
kgh <rjsgml302@naver.com>
2023-03-17 02:31:39 +0900

    next morning

--- src/redux/store_component/order.tsx
+++ src/redux/store_component/order.tsx
@@ -57,18 +57,19 @@ const orderSlice = createSlice({
   initialState : initialOrderSlice,
   reducers : {
     itemListSet (state, action:PayloadAction<number>){
+      //초기화
       state.itemValues = []
+      state.itemTempArr = []
+      //
       let arrIndex : number = 4
       let itemLength : number = state.items.item.length
-
       state.items.item.map((a)=> {state.itemTempArr.push({[state.items.subMenu] : a})})
-
+      //실작업
       for (let i : number = 0; i <itemLength; i += arrIndex){
         let blockItem : {[key:string] :standardItem}[]
         blockItem = state.itemTempArr.slice(i, i+arrIndex)
         state.itemValues.push(blockItem)
       }
-      
     }
   }
 })
