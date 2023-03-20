ea7caf17780d61c3f05a38326bbba8afcc2bd244
kgh <rjsgml302@naver.com>
2023-03-21 03:27:45 +0900

    cart all remake

--- src/components/main/cart_component/Cart.tsx
+++ src/components/main/cart_component/Cart.tsx
@@ -12,81 +12,42 @@ export const Cart = () => {
   return(
     <React.Fragment>
       <div className={Styled.cart_contents}>
-        <div className={Styled.cart_table_area}>
-          <table className={Styled.cart_table}>
-            <thead>
-              <tr className={Styled.thead_title}>
-                <th> 상품 </th>
-                <th> 개수 </th>
-                <th> 금액 </th>
-                <th>     </th>
-              </tr>
-            </thead>
-            {
-              itemValue.map((e, i) => {
-                let itemType :string = e.id[0]==='p' ? 'pudding' : '';
+        <div className={Styled.cart_list_area}>
+          {
+            itemValue.map((e, i) => {
+              let itemTitle = e.id[0] === 'p' ? 'pudding' : null
 
-                return (
-                  <tbody key={i}>
-                    <tr className={Styled.cart_in_item}>
-                        <td className={Styled.img_title_infor}> 
-                          <div className={Styled.table_body_item}>
-                            <span>
-                              <img 
-                                className={Styled.cart_item_img}
-                                src={process.env.PUBLIC_URL + `/item/${itemType}/${e.pName}.png`} 
-                                alt='item_custard'/>
-                            </span>
-                            <span>
-                              <h4 className={Styled.titleName}> {`${e.pName.toUpperCase()} ${itemType.toUpperCase()}`} </h4>
-                              <div className={Styled.costInfor}>
-                                <p> per piece</p>
-                                <p> 
-                                  <span> ₩ </span>
-                                  <span> {e.price} </span>
-                                </p>
-                              </div>
-                            </span>
-                          </div>
-                        </td>
-                        <td>
-                          <div className={Styled.table_body_amount}>
-                            <div>
-                              <button>-</button>
-                              <span> {e.count} </span>
-                              <button>+</button>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <div className={Styled.table_body_item_cost}>
-                            <div>
-                              <span> ₩ </span>
-                              <span> {e.price * e.count} </span>
-                            </div>
-                          </div>
-                        </td>
-                        <th>
-                          <button> X </button>
-                        </th>
-                    </tr>
-                  </tbody>
-                )
-              })
-            }
-          </table>
-        </div>
-        <div className={Styled.cart_sticky_area}>
-          <div className={Styled.cart_bot_content}>
-            <div className={Styled.cart_bot_total}>
-              <p>
-                Total ₩ 1,000,000
-              </p>
-            </div>
-            <div className={Styled.cart_bot_payment}>
-              <button >주문</button>
-            </div>
-          </div>
+              return (
+                <div key={i} className={Styled.cart_in_item}>
+                  <div className={Styled.cart_img_area}>
+                    <img src={process.env.PUBLIC_URL+ `/item/${itemTitle}/${e.pName}.png`} 
+                      alt={e.id} className={Styled.cart_item_img}/>
+                  </div>
+                  <div className={Styled.cart_item_title}>
+                    <h4> {`${e.pName.toUpperCase()} ${itemTitle?.toUpperCase()}`} </h4>
+                    <div className={Styled.item_cost_infor}>
+                      <p> - per piece </p>
+                      <span> ₩  {e.price} </span>
+                    </div>
+                  </div>
+                  <div className={Styled.cart_item_count}>
+                    { e.count.toString().padStart(2,'0') }
+                    <div>
+                      <button>+</button>
+                      <button>-</button>
+                    </div>
+                  </div>
+                  <div className={Styled.cart_item_total}>
+                    <p> total </p>
+                    <span>₩ : </span>
+                    <span>{ e.count * e.price}</span>
+                    <button className={Styled.cart_item_delete}>X</button>
+                  </div>
+
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </React.Fragment>
